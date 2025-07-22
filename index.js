952066dec7b16d53b9afd1a999ab7cbe9a5cd296
Wallace de Souza Pereira Campos <wpereirario@gmail.com>
2025-07-22 18:26:53 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+var criativo1 = window.document.getElementById("criativo1")
+var criativo2 = window.document.getElementById("criativo2")
+var planos = window.document.getElementById("planos")
+
+function RolarParaDireita() {
+    criativo1.style = "display:none"
+    planos.style = "display:flex"
+};
+
